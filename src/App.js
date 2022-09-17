20ae7f3325736945068a108ffe2d21502a69a247
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-09-17 20:40:54 +0530

    created user-onboarding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import NewTab from "./pages/NewTab";
 import './app.css'
+import UserOnboarding from "./pages/userOnboarding/UserOnboarding";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [name,setName] = useState(null)
+
+  useEffect(() => {
+    setName(localStorage.getItem('tabify-name'))
+  },[])
+
   return (
     <div className='app'>
-      <NewTab/>
+      {name === null
+        ? (
+          <UserOnboarding/>
+          )
+        : (
+          <NewTab/>
+        )
+      }
     </div>
   );
 }
---
+++ src/pages/userOnboarding/UserOnboarding.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import './userOnboarding.css'
+
+const UserOnboarding = () => {
+
+    const [name,setName] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        localStorage.setItem('tabify-name', name)
+        window.location.reload(false)
+    }
+
+  return (
+    <div className='tabify-onboarding'>
+      <p className='tabify-onboarding-text'>Hey, What Should We Call You?</p>
+      <form onSubmit={handleSubmit}>
+        <input type='text' className='tabify-onboarding-input' value={name} onChange={(e) => setName(e.target.value)}/>
+      </form>
+    </div>
+  )
+}
+
+export default UserOnboarding
